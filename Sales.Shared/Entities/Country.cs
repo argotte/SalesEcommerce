bcf652e20f36702b29346cd77576b260b135243a
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-02-21 05:06:46 -0400

    Add DataContext

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        public string Name { get; set; } = null!;
+
+    }
+}
