11c01ca8a3059593b49a934b7e8b6442c7f3cb47
diegoargottez@gmail.com <diegoargottez@gmail.com>
2023-02-24 02:56:56 -0400

    Countries crud  started in web. Only get list. Nothing else.

--- Sales.Web/Program.cs
+++ Sales.Web/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.Web;
+using Sales.Web.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7138//") });
+
+builder.Services.AddScoped<IRepository, Repository>();
+//scoped: quiero q me cree una nueva instancia del objeto cada vez q llamo inyeccion
+//transient: lo inyecta solo una vez. 
+//singleton: crea la isntancia y luego la llama repetidas veces. No recomendado
+
 
 await builder.Build().RunAsync();
---
+++ Sales.Web/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+
+namespace Sales.Web.Repositories
+{
+    //Generic class
+    //Generic response for https responses
+    //This is copied and pasted from Zulu's course
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public bool Error { get; set; }
+
+        public T? Response { get; set; }
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public async Task<string?> GetErrorMessage()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var codigoEstatus = HttpResponseMessage.StatusCode;
+            if (codigoEstatus == HttpStatusCode.NotFound)
+            {
+                return "RESOURCE NOT FOUND";
+            }
+            else if (codigoEstatus == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            else if (codigoEstatus == HttpStatusCode.Unauthorized)
+            {
+                return "You need to login to keep going forward.";
+            }
+            else if (codigoEstatus == HttpStatusCode.Forbidden)
+            {
+                return "You are not allowed to continue with this operation";
+            }
+
+            return "An unexpected error has occurred";
+        }
+    }
+
+}
